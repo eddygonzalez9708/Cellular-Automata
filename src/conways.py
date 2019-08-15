6287b27a795c0c2a7ca13a65de0123c186812e18
Eddy <edward.gonzalez9708@gmail.com>
2019-08-15 19:58:41 -0400

    Finished watching half of the guided demo on Conway's Game of Life part 2.

--- src/conways.py
+++ src/conways.py
@@ -6,40 +6,95 @@ WHITE = (255, 255, 255)
 GRAY = (128, 128, 128)
 WIN_SIZE = 500
 
+# Allows users to set the number of squares.
+
+# NUM_SQUARES = ??
+# WIN_SIZE = NUM_SQUARES * 20 + (NUM_SQUARES + 1) * 5
+
 # 1. Create an initial set of states with simiple pattern (Ex. blinker)
 
 cur_state = [00] * 500
-cur_state[10] = 1
-cur_state[20] = 1
-cur_state[30] = 1
-cur_state[40] = 1
-cur_state[50] = 1
 
 next_state = []
+
+# 1 v.2 Fill cur state with random states
+
+for i in range(len(cur_state)):
+  cur_state[i] = random.randint(0, 1)
+
+# 1 v.3 Allow users to choose between several predefined initial states.
 
 pygame.init()
  
 # Set the width and height of the screen [width, height]
+
 size = (WIN_SIZE, WIN_SIZE)
 screen = pygame.display.set_mode(size)
 
 # Add a title
+
 pygame.display.set_caption("Conway's Game of Life")
  
 # Loop until the user clicks the close button.
+
 done = False
  
 # Used to manage how fast the screen updates
+
 clock = pygame.time.Clock()
  
 # -------- Main Program Loop -----------
+
 while not done:
   # --- Main event loop
+  
   for event in pygame.event.get():
     if event.type == pygame.QUIT:
       done = True
  
   # --- Transition logic should go here
+
+  # Calculate the number of live neighbors
+  # a. Find the indexes of all neighbors
+
+  # Example:
+  ##########
+  '''
+  -------
+  |0|3|6|
+  |1|4|7|
+  |2|5|8|
+  -------
+  '''
+
+  # index = current cell
+  # east = index + width
+  # west = index - width
+  # north = index - 1
+  # south = index + 1
+  # north_east = n + width
+  # north_west = n - width
+  # south_east = s + width
+  # south_west = s - width
+
+  # live_neighbors = 0
+  # if cur_state[north] == 1:
+  #   live_neighbors += 1
+  # if cur_state[east] == 1:
+  #   live_neighbors +=1
+  # if cur_state[south] == 1:
+  #   live_neighbors +=1
+  # if cur_state[west] == 1:
+  #   live_neighbors +=1
+  # if cur_state[north_east] == 1:
+  #   live_neighbors +=1
+  # if cur_state[north_west] == 1:
+  #   live_neighbors +=1
+  # if cur_state[south_east] == 1:
+  #   live_neighbors +=1
+  # if cur_state[south_west] == 1:
+  #   live_neighbors +=1
+  ##########
 
   # Any live cell with fewer than two live neighbours dies, as if by underpopulation.
   # Any live cell with two or three live neighbours lives on to the next generation.
